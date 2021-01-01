0f5fbb9e445c4f414ba09a75f514ab3cbf7a1531
Steffen Köhler <steffen@koehlers.de>
2021-01-01 17:41:20 +0100

    added kodi ip and player_id as optional parameters

--- helper_tools/kodi_rpc_test.py
+++ helper_tools/kodi_rpc_test.py
@@ -3,17 +3,25 @@ import json
 import sys
 
 def main():
-    url = "http://192.168.1.127:8080/jsonrpc"
+    if len(sys.argv)>2:
+        url = 'http://'+sys.argv[2]+':8080/jsonrpc'
+    else:
+        url = "http://192.168.1.127:8080/jsonrpc"
+    if len(sys.argv)>3:
+        player_id = int(sys.argv[3])
+    else:
+        player_id = 1
     #url = "http://kodiwohnzimmer.fritz.box:8080/jsonrpc"
     #url = "http://kodi-wohnzimmer.fritz.box:8080/jsonrpc"
     #url = "http://192.168.2.30:8080/jsonrpc"
     #url = "http://192.168.1.94:8080/jsonrpc"
 
     # Example echo method
-    payload ={"jsonrpc":"2.0", "id":1, "method": "Player.Open", "params":{"item":{"file":sys.argv[1]}}}
+    payload ={"jsonrpc":"2.0", "id" : player_id, "method": "Player.Open", "params":{"item":{"file":sys.argv[1]}}}
     
     response = requests.post(url, json=payload).json()
 
+    print(url, repr(payload))
     print(response)
 
 if __name__ == "__main__":
