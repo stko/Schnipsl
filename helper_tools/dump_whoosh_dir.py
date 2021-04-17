9cfa3ce905da7d1f4f915f1c48dfb0dacd97f06d
Steffen Köhler <steffen@koehlers.de>
2021-04-17 14:29:03 +0200

    output sorted by timestamp

--- helper_tools/dump_whoosh_dir.py
+++ helper_tools/dump_whoosh_dir.py
@@ -1,10 +1,15 @@
 from whoosh.index import open_dir
 #from whoosh.query import Every
 
+sorted_data = {}
+
 ix = open_dir('indexdir')
 #results = ix.searcher().search(Every('uri'))
 all_docs = ix.searcher().documents()
 for doc in all_docs:
-	for f in doc.keys():
-		print(f'index {f} : {doc[f]}')
-
+    sorted_data[doc['uri']] = doc
+for sorted_uri in sorted(sorted_data.keys()):
+    doc = sorted_data[sorted_uri]
+    for f in doc.keys():
+        print(f'index {f} : {doc[f]}')
+    print('-------------')
