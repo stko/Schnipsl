9e0ef92f942b15720e3b57272c61b7971042103c
Steffen Köhler <steffen@koehlers.de>
2021-07-02 17:36:09 +0200

    moved to right position

--- devices/master/plugins/channels_satip/installdocker.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-cd $SCRIPT_DIR
-apt-get update
-apt-get install -y nodejs npm python3-xmltodict libyajl-dev
-npm install dvbtee
-pip install jsonslicer xmltodict
--- devices/master/plugins/epg_sat/installdocker.sh
+++ devices/master/plugins/epg_sat/installdocker.sh
@@ -2,6 +2,6 @@
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 cd $SCRIPT_DIR
 apt-get update
-apt-get install -y nodejs npm python3-xmltodict libyajl-dev
+apt-get install -y nodejs npm python3-xmltodict libyajl-dev curl ffmpeg
 npm install dvbtee
 pip install jsonslicer xmltodict
