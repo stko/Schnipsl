721dffe7783cee3e1997a11cb1f1659c81d4f599
Steffen Köhler <steffen@koehlers.de>
2022-04-15 16:47:20 +0200

    latest docker linux image had new packet structure for node

--- devices/master/plugins/epg_sat/installdocker.sh
+++ devices/master/plugins/epg_sat/installdocker.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 cd $SCRIPT_DIR
-$SUDO apt-get install -y node npm python3-xmltodict libyajl-dev curl ffmpeg
+$SUDO apt-get install -y curl gnupg
+curl -sL https://deb.nodesource.com/setup_14.x  | $SUDO bash -
+$SUDO apt-get install -y nodejs python3-xmltodict libyajl-dev curl ffmpeg
 npm install dvbtee
 $SUDO pip3 install jsonslicer xmltodict
-
